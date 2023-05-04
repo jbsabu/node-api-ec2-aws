7ee35ca6e54fa2c5b78120306888d710802ce86c
Jonathan B. Sabuncu <sundfo057@gmail.com>
2023-05-04 15:47:34 -0400

    Dogs

--- index.js
+++ index.js
@@ -6,10 +6,33 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+
+const dogs = [
+  {
+    name: 'Paco',
+    breed: 'Chihuaha',
+    age: 23
+  },
+  {
+    name: 'Buba',
+    breed: 'Chihuaha',
+    age: 23
+  },
+  {
+    name: 'Bob',
+    breed: 'Chihuaha',
+    age: 23
+  },
+  {
+    name: 'Lucia',
+    breed: 'Chihuaha',
+    age: 23
+  }
+]
 // routes
 
 app.get("/path", (req, res) => {
-  res.send('welcome to my API');
+  res.send(dogs);
 });
 
 app.listen(4000, () => console.log(`listening on port 4000`));
